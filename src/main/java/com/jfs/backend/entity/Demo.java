b168e9db3487f8f87f5f140a43ef76a810b60209
Kartik K <krkhengate123@gmail.com>
2024-01-12 11:02:16 +0530

    fix bug121

--- src/main/java/com/jfs/backend/entity/Demo.java
+++ src/main/java/com/jfs/backend/entity/Demo.java
@@ -2,9 +2,15 @@ package com.jfs.backend.entity;
 
 public class Demo {
 
+<<<<<<< Updated upstream
     public static void main(String[] args) {
 
         System.out.printf("bug 132");
     }
+=======
+    private String name;
+
+    private int age;
+>>>>>>> Stashed changes
 
 }
