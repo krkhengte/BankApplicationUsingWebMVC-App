f33a8a7b0ef5391c35400072f039acced0f56419
Kartik K <krkhengate123@gmail.com>
2024-01-12 10:34:59 +0530

    bug122 commit

---
+++ index.java
@@ -0,0 +1,11 @@
+public class index{
+	
+	public static void main(String[] args){
+		
+		System.out.println("First Change");
+		
+		System.out.pritln("Second Change with out commiting first commit");
+		
+	}
+	
+}
