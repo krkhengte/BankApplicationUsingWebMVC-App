4c26d2e0ee476ba2a17b2b8841ada3e703ba12ec
Kartik K <krkhengate123@gmail.com>
2024-01-12 10:40:55 +0530

    updated bug123

--- index.java
+++ index.java
@@ -2,9 +2,9 @@ public class index{
 	
 	public static void main(String[] args){
 		
-		System.out.println("First Change without commit stop doing changes");
-		
 		System.out.println("second Change with commit to complete changes");
+		
+		
 		
 		
 		
