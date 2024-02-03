b52abd49d7b840134a7e7718ba455066aeedeffe
Kartik K <krkhengate123@gmail.com>
2024-02-03 14:37:00 +0530

    test file commented

--- src/test/java/com/example/demo/BankApplicationUsingWebMvcApplicationTests.java
+++ src/test/java/com/example/demo/BankApplicationUsingWebMvcApplicationTests.java
@@ -1,16 +1,16 @@
-package com.example.demo;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import com.jfs.backend.BankApplicationUsingWebMvcApplication;
-
-@SpringBootTest(classes = BankApplicationUsingWebMvcApplication.class)
-class BankApplicationUsingWebMvcApplicationTests {
-
-	@Test
-	void contextLoads() {
-		
-	}
-
-}
+//package com.example.demo;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import com.jfs.backend.BankApplicationUsingWebMvcApplication;
+//
+//@SpringBootTest(classes = BankApplicationUsingWebMvcApplication.class, excludeAutoConfiguration = {AutoConfiguration.class})
+//class BankApplicationUsingWebMvcApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//		
+//	}
+//
+//}
