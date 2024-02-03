a680a918c04ba0349bb9050a95613d67451c8a40
Kartik K <krkhengate123@gmail.com>
2024-02-03 13:58:03 +0530

    test file updated

--- src/test/java/com/example/demo/BankApplicationUsingWebMvcApplicationTests.java
+++ src/test/java/com/example/demo/BankApplicationUsingWebMvcApplicationTests.java
@@ -3,11 +3,14 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.jfs.backend.BankApplicationUsingWebMvcApplication;
+
+@SpringBootTest(classes = BankApplicationUsingWebMvcApplication.class)
 class BankApplicationUsingWebMvcApplicationTests {
 
 	@Test
 	void contextLoads() {
+		
 	}
 
 }
